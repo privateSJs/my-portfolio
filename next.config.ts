ede3719d0f3e9ac7f5d3b0a4ca2c28633bd66fba
Jarosław Stysiał <jarek9917@gmail.com>
2025-01-24 06:35:06 +0100

    config changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
     trailingSlash: true,
+  basePath: isProd ? "/my-portfolio" : "",
+  assetPrefix: isProd ? "/my-portfolio/" : "",
 };
 
 export default nextConfig;
